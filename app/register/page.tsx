a8d1ba3c7d89c82b7ad7b02f79b451b919a7d65b
Ahtishamkayani01 <ahtishamkayani01@gmail.com>
2026-09-12 15:06:58 +0500

    Fixed mobile view of app

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,7 @@ import Register from "../components/register";
 
 export default function RegisterPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between md:p-24">
       <Register />
     </div>
   );
